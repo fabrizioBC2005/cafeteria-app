919834f9f6458de26e8ed3994535c0a3b300734a
fabrizio Coding <fabrizio23enero@gmai.com>
2026-03-11 00:52:12 -0500

    homecss-appss feature

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,24 +4,28 @@ function Navbar() {
   return (
     <nav className="navbar">
 
-      <div className="logo">
-        <h2>PUMA CAFÉ</h2>
-      </div>
+      <div className="navbar-container">
 
-      <ul className="menu">
-        <li>Inicio</li>
-        <li className="sale">SALE</li>
-        <li>Tienda</li>
-        <li>Cafetería</li>
-        <li>Tostaduría</li>
-        <li>Contáctanos</li>
-        <li>Historia</li>
-        <li>Blog</li>
-      </ul>
+        <div className="logo">
+          <h2>CAFETERÍA</h2>
+        </div>
 
-      <div className="icons">
-        <span>🔍</span>
-        <span>🛒</span>
+        <ul className="menu">
+          <li>Inicio</li>
+          <li>Tienda</li>
+          <li>Cafetería</li>
+          <li>Tostaduría</li>
+          <li>Historia</li>
+          <li>Blog</li>
+          <li>Contáctanos</li>
+          <li className="sale">SALE</li>
+        </ul>
+
+        <div className="icons">
+          <span>🔍</span>
+          <span>🛒</span>
+        </div>
+
       </div>
 
     </nav>
